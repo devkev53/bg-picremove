ff0cc5cdcc9b57df9871a9481df1fedf02814f44
kevin palma <kapalma05@gmail.com>
2023-03-02 21:53:21 -0600

    :construction: working in home page
    home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,9 @@ import { Link } from 'wouter';
 import exampleVideo from '../../assets/example.mp4'
 import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import WallpaperIcon from '@mui/icons-material/Wallpaper';
+import DownloadingIcon from '@mui/icons-material/Downloading';
 
 import styles from './styles.module.css'
 
@@ -10,7 +13,7 @@ const index = () => {
     <div className={styles.homeContainer}>
       <main className={styles.homeMain}>
         <article className={styles.articleOne}>
-          <picture className={styles.homeLogo}><span>BG-PicRemove</span></picture>
+           <picture className={styles.homeLogo}><span>BG-PicRemove</span></picture>
           <div className={styles.homeText}>
             <h3>Remove the backgroun with this app</h3>
             <p>Easily remove the background from your images and edit them with this web application, thanks to the artificial intelligence provided by <a href="">cloudinary</a></p>
@@ -28,7 +31,50 @@ const index = () => {
         </article>
 
         <article className={styles.articleTwo}>
-          <div></div>
+          <div className={styles.containerArticle2}>
+            <h3>
+              How to use the 
+              <picture>
+                <span>BG-PicRemove</span>
+              </picture>
+            </h3>
+
+            <div className={styles.cardsContainer}>
+
+              <div className={styles.cardGuide}>
+                <div className="icon">
+                  <UploadFileIcon sx={{ fontSize: 60 }} />
+                </div>
+                <div className="text">
+                  <h3>1. Upload</h3>
+                  <p>For best results, the subject or object you want to remove the background from should have well-defined edges.</p>
+                </div>
+              </div>
+
+              <div className={styles.cardGuide}>
+                <div className="icon">
+                  <WallpaperIcon sx={{ fontSize: 60 }} />
+                </div>
+                <div className="text">
+                  <h3>2. Remove</h3>
+                  <p>When you upload your image, cloudinary's artificial intelligence automatically removes the background.</p>
+                </div>
+              </div>
+
+              <div className={styles.cardGuide}>
+                <div className="icon">
+                  <DownloadingIcon sx={{ fontSize: 60 }} />
+                </div>
+                <div className="text">
+                  <h3>3. Download</h3>
+                  <p>
+  Download your new image with a transparent background, save it, share it or add more effects</p>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
         </article>
       </main>
     </div>
